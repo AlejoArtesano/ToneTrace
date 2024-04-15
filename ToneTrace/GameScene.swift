30e4e06210d53e51fa187fb1711ca56f18c6b743
Alejo Artesano <45542827+AlejoArtesano@users.noreply.github.com>
2024-04-15 19:26:04 +0400

    Adjusted layout for game interface setup

--- ToneTrace/GameScene.swift
+++ ToneTrace/GameScene.swift
@@ -2,11 +2,32 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    var squares = [SKShapeNode]()
     
     override func didMove(to view: SKView) {
-        
+      backgroundColor = SKColor.black
+      setupSquares()
+      
     }
     
+  
+  func setupSquares() {
+    let colors = [
+      SKColor(red: 1, green: 0.5, blue: 0.5, alpha: 1), // более светлый красный
+      SKColor(red: 1, green: 1, blue: 0.5, alpha: 1), // более светлый желтый
+      SKColor(red: 0.5, green: 1, blue: 0.5, alpha: 1) // более светлый зеленый
+    ]
+    let squareSize = CGSize(width: 80, height: 80)
     
+    for i in 0..<3 {
+      let square = SKShapeNode(rectOf: squareSize, cornerRadius: 12) // Увеличенные скругленные углы
+      square.fillColor = colors[i]
+      square.position = CGPoint(x: frame.midX + CGFloat(i * 100) - 100, y: frame.midY) // Расширение расстояния между квадратами
+      square.name = "square\(i)"
+      squares.append(square)
+      addChild(square)
+    }
+  }
+  
+  
 }
