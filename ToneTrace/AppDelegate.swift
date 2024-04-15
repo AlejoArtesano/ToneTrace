676d947fdd6c428ca92182cc8dc09029d0c686a2
Alejo Artesano <45542827+AlejoArtesano@users.noreply.github.com>
2024-04-15 19:03:45 +0400

    Initial Commit

---
+++ ToneTrace/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ToneTrace
+//
+//  Created by Aleksei Kapranov on 15.04.2024.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
