ebb9d0d5590622d4c84faf9bad491ce4ea613161
Alejo Artesano <45542827+AlejoArtesano@users.noreply.github.com>
2024-04-17 23:52:24 +0400

    Added sound playback functionality and refactor game code

--- ToneTrace/GameScene.swift
+++ ToneTrace/GameScene.swift
@@ -1,20 +1,67 @@
 import SpriteKit
 import GameplayKit
+import AVFoundation
+
 
 class GameScene: SKScene {
     var squares = [SKShapeNode]()
     var sequence = [Int]()
     var userSequence = [Int]()
 
-
+    var audioPlayer1: AVAudioPlayer?
+    var audioPlayer2: AVAudioPlayer?
+    var audioPlayer3: AVAudioPlayer?
+  
     override func didMove(to view: SKView) {
       backgroundColor = SKColor.black
+      setupAudioPlayers()
       setupSquares()
       generateSequence(length: 3)  // Генерируем последовательность из 3 миганий
       showSequence()  // Показываем её пользователю
       
     }
     
+  
+  func setupAudioPlayers() {
+    audioPlayer1 = createAudioPlayer(fileName: "guitar_a2_A.m4a")
+    audioPlayer2 = createAudioPlayer(fileName: "guitar_b2_B.m4a")
+    audioPlayer3 = createAudioPlayer(fileName: "guitar_f2-low_F.m4a")
+  }
+  
+  private func createAudioPlayer(fileName: String) -> AVAudioPlayer? {
+    guard let url = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+      print("Failed to find the file \(fileName)")
+      return nil
+    }
+    do {
+      let player = try AVAudioPlayer(contentsOf: url)
+      player.prepareToPlay()
+      return player
+    } catch {
+      print("Failed to create audio player: \(error)")
+      return nil
+    }
+  }
+  
+  
+  func setupSquares() {
+    let colors = [
+      SKColor(red: 1, green: 0.5, blue: 0.5, alpha: 1), // более светлый красный
+      SKColor(red: 1, green: 1, blue: 0.5, alpha: 1), // более светлый желтый
+      SKColor(red: 0.5, green: 1, blue: 0.5, alpha: 1) // более светлый зеленый
+    ]
+    let squareSize = CGSize(width: 80, height: 80)
+    
+    for i in 0..<3 {
+      let square = SKShapeNode(rectOf: squareSize, cornerRadius: 12) // Увеличенные скругленные углы
+      square.fillColor = colors[i]
+      square.position = CGPoint(x: frame.midX + CGFloat(i * 100) - 100, y: frame.midY) // Расширение расстояния между квадратами
+      square.name = "square\(i)"
+      squares.append(square)
+      addChild(square)
+    }
+  }
+  
   func generateSequence(length: Int) {
     sequence.removeAll()
     for _ in 0..<length {
@@ -38,24 +85,6 @@ class GameScene: SKScene {
   }
 
   
-  func setupSquares() {
-    let colors = [
-      SKColor(red: 1, green: 0.5, blue: 0.5, alpha: 1), // более светлый красный
-      SKColor(red: 1, green: 1, blue: 0.5, alpha: 1), // более светлый желтый
-      SKColor(red: 0.5, green: 1, blue: 0.5, alpha: 1) // более светлый зеленый
-    ]
-    let squareSize = CGSize(width: 80, height: 80)
-    
-    for i in 0..<3 {
-      let square = SKShapeNode(rectOf: squareSize, cornerRadius: 12) // Увеличенные скругленные углы
-      square.fillColor = colors[i]
-      square.position = CGPoint(x: frame.midX + CGFloat(i * 100) - 100, y: frame.midY) // Расширение расстояния между квадратами
-      square.name = "square\(i)"
-      squares.append(square)
-      addChild(square)
-    }
-  }
-  
   func highlightSquare(at index: Int) {
     let highlightAction = SKAction.sequence([
       SKAction.scale(to: 1.2, duration: 0.1),
@@ -74,11 +103,28 @@ class GameScene: SKScene {
         if square.frame.contains(location) {
           highlightSquare(at: index)
           userSequence.append(index)
+          playSoundForSquare(index: index)
           checkCompletion()  // Проверяем, завершил ли пользователь ввод
         }
       }
     }
   }
+  
+  
+  func playSoundForSquare(index: Int) {
+    print("Attempting to play sound for square at index \(index)")
+    switch index {
+      case 0:
+        audioPlayer1?.play()
+      case 1:
+        audioPlayer2?.play()
+      case 2:
+        audioPlayer3?.play()
+      default:
+        print("No audio player available for index \(index)")
+    }
+  }
+
   
   func checkCompletion() {
     if userSequence.count == sequence.count {
@@ -87,6 +133,5 @@ class GameScene: SKScene {
 
     }
   }
-  
   
 }
