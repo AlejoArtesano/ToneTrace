39e00fe08958f99be9dab670d0c7d1b9cea82f9a
Alejo Artesano <45542827+AlejoArtesano@users.noreply.github.com>
2024-04-24 23:47:18 +0400

    Removed unused Storyboard, ViewController, and GameScene.sks; added main.swift; refactored AppDelegate to use SKView directly.

--- ToneTrace/AppDelegate.swift
+++ ToneTrace/AppDelegate.swift
@@ -1,24 +1,30 @@
-//
-//  AppDelegate.swift
-//  ToneTrace
-//
-//  Created by Aleksei Kapranov on 15.04.2024.
-//
-
-
 import Cocoa
+import SpriteKit
 
-@main
 class AppDelegate: NSObject, NSApplicationDelegate {
+  var window: NSWindow!
+  var skView: SKView!
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Создаем окно
+    window = NSWindow(
+      contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+      styleMask: [.titled, .closable, .miniaturizable, .resizable],
+      backing: .buffered,
+      defer: false)
+    window.center()
+    window.title = "ToneTrace Game"
     
+    // Создаем SKView
+    skView = SKView(frame: NSRect(x: 0, y: 0, width: 800, height: 600))
+    window.contentView = skView
     
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-    
-    
+    // Загружаем и отображаем GameScene
+    let scene = GameScene(size: skView.bounds.size)
+    scene.scaleMode = .aspectFill
+    skView.presentScene(scene)
+        
+    window.makeKeyAndOrderFront(nil)
+  }
 }
+
